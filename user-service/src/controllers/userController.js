93bf2edd111eb14310d34aed13d25ac2cab92923
Cristiam Vargas <kristianva@gmail.com>
2025-02-04 11:55:54 -0600

    test commit for review service

--- user-service/src/controllers/userController.js
+++ user-service/src/controllers/userController.js
@@ -19,16 +19,17 @@ const registerUser = async (req, res) => {
 // Login user
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ email });
-        if (!user || !(await bcrypt.compare(password, user.password))) {
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.json({ token });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    // TODO: uncomment later
+    // try {
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({ message: 'Invalid credentials' });
     }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+    // } catch (error) {
+    //     res.status(500).json({ message: error.message });
+    // }
 };
 
    // Update User Username
